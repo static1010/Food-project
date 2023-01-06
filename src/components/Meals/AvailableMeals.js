462fe4e78e264a3ea7b7136935f7ce14a2f05b66
Bozhidar Gorchev <bgorchev@aiopsgroup.com>
2023-01-06 11:59:21 +0200

    add modal(react portal) in project

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -30,12 +31,21 @@ const DUMMY_MEALS = [
 ];
 
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
-      <Card>
+    <Card>
       <section className={classes.meals}>
-      <ul>{mealsList}</ul>
-    </section></Card>
+        <ul>{mealsList}</ul>
+      </section>
+    </Card>
   );
 }
